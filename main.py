62b9f5aac0740e2fcc18e166c5f8517042477a5e
Taksh Rana <ranataksh03@gmail.com>
2023-12-11 23:42:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", 'sqlite:///todo.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("CYCLIC_DB", 'sqlite:///todo.db')
 db = SQLAlchemy()
 db.init_app(app)
 
