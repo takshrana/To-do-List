7bb91fe1bb9ea746ca9e13a0fcd6a103849348b6
Taksh Rana <ranataksh03@gmail.com>
2023-12-11 23:23:33 +0530

    Update main.py
    added init app

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", 'sqlite:///posts.db')
 db = SQLAlchemy(app)
+db.init_app(app)
 
 
 class Todo(db.Model):
